b71068c5d706921f341e25ab2123efc4bdc722c8
JohnsonHuang <huangqihao@xh-online.com>
2023-11-23 14:48:40 +0800

    chore: upgrade main.ts

--- bin/main.ts
+++ bin/main.ts
@@ -1,14 +1,22 @@
 #!/usr/bin/env node
 
-import pkg from '../package.json' assert { type: 'json' }
+import fs from 'node:fs'
 import path from 'node:path'
 import { program as creator } from 'commander'
 import { installProgram } from './utils/install.js'
 
 const dirname = new URL('.', import.meta.url).pathname.replace(/\/*/, '')
 
+function getPackageVersion() {
+  const packageJSONContent = fs.readFileSync(
+    path.join(dirname, '../package.json'),
+    'utf-8'
+  )
+  return JSON.parse(packageJSONContent).version || '0.0.0'
+}
+
 async function initProgram() {
-  creator.version(pkg.version)
+  creator.version(getPackageVersion())
 
   /* Auto load modules from src dir, and register in program */
   const programList = await installProgram(path.join(dirname, './commands'))
